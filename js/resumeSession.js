3f75ae6a34d7cc83ce173408afdeb41ba8fcd911
athanasios44 <aantoniou16@uclan.ac.uk>
2026-01-16 10:18:17 +0200

    resumeSession.js

--- js/resumeSession.js
+++ js/resumeSession.js
@@ -5,57 +5,51 @@ import {
 } from "./api.js";
 
 //start new session from API and save it in local storage
-async function startNewSession() {
-    const start = await startSession(player , app , treasureHuntId);
-    const ID = start.sessionId;
-    let saveSession = localStorage.setItem("Session Id", ID);
-    LoadNextQuestion(ID);
+export async function startNewSession(player,app,treasureHuntId) {
+    const start = await startSession(player,app,treasureHuntId);
+    const id = start.sessionId;
+    let saveSession = localStorage.setItem("SessionId",JSON.stringify(id));
+    await LoadNextQuestion(id);
 }
 
 //Every time window loads check if a session is present
-window.onload = function reloadPage() {
+window.onload =async function reloadPage() {
         //Save it
-        const loadSavedSession = localStorage.getItem("Session Id");
+        let loadSavedSession = localStorage.getItem("SessionId");
         //No session found throw error
-        if (!loadSavedSession)
+        if (!loadSavedSession){
             console.log("Error no Session Id Found");
-        else
-            //Load the question
-            LoadNextQuestion(loadSavedSession);
+        }
+        //Load the question
+        else{
+            const session = JSON.parse(loadSavedSession);
+            await LoadNextQuestion(session);
+        }
 
 }
 
 //Function that loads the next question from API
-async function LoadNextQuestion(ID) {
+export async function LoadNextQuestion(id) {
     try{
         //save question in variable
-        let question = await fetchQuestion(ID);
+        let question = await fetchQuestion(id);
         //If no question found
-        if (!question)
+        if (!question){
             console.error("Error fetching Question");
+        }
         //If all questions are completed delete the Session From the local storage
         else if(question.completed){
-            localStorage.removeItem("Session Id", ID);
-            gameFinished();
+            console.log("Congratulations Hunter! You completed the Treasure Hunt!");
             return;
         }
         //Show the next question
-        else
-            showQuestion(question);
+        else{
+            console.log(question);
+            console.log(question.questionText);
+        }
     }
     //Display Error
     catch (error) {
         console.error("Unknown Error");
     }
-}
-
-//Show Finish message
-function gameFinished() {
-    console.log("Congratulations! , Treasure Hunt Completed! ");
-}
-
-//Display the next question from API
-function showQuestion(q) {
-    console.log(q);
-    console.log(q,q.questionText);
 }
