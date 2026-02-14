13bb1b88487757331dc993d8596b1f0e79f8b1c6
athanasios44 <aantoniou16@uclan.ac.uk>
2026-02-14 14:46:51 +0200

    changes made
    removed leaderboard concept . score.js will only work inside the html page where the treasure hunt will take place .

--- js/score.js
+++ js/score.js
@@ -1,35 +1,44 @@
-//import functions from different js files
-import {
-    findAndSaveSessionId,
-    LoadNextQuestion
-}from "./resumeSession.js";
+//Needs to only work when the game starts so only the html page that the hunt takes place
+if(window.location.pathname.includes("")) {
+    //import functions from different js files
+    import {
+        findAndSaveSessionId,
+    } from "/resumeSession.js";
 
 //import functions
-import {
-    CheckAndDisplay
-}from "./sessionEnd.js";
+    import {
+        CheckAndDisplay
+    } from "/sessionEnd.js";
 
-import {
-    fetchScore,
-} from "./api.js";
+    import {
+        fetchScore,
+    } from "/api.js";
 
 //use findAndSaveSessionId to save the sessionId using local storage
-let sessionId = await findAndSaveSessionId();
-
-
-export async function updateScore(sessionId){
-    //if CheckAndDisplay function returns false
-    let game_finished = await CheckAndDisplay(sessionId);
-    if(!game_finished){
-        //fetch score from API
-        let score = await fetchScore(sessionId);
-        //Display it
-        console.log("score: ", score);
-        //No actual displays yet only in the console log
-        //
-        //document.getElementById(" ").innerHTML = score;
-        //call the next question
-        await LoadNextQuestion(sessionId);
+    let sessionId = await findAndSaveSessionId();
+    if(sessionId === null) {
+        console.error("No such sessionId");
+    }
+    else if(!await CheckAndDisplay(sessionId) ){
+       await updateScore(sessionId);
     }
 
+    export async function updateScore(sessionId) {
+        //if CheckAndDisplay function returns false
+        let game_finished = await CheckAndDisplay(sessionId);
+        if (!game_finished) {
+            //fetch score from API
+            let score = await fetchScore(sessionId);
+            //Display it
+            console.log("score: ", score.score);
+            //No actual displays yet only in the console log
+            //use this inside the html to display
+            //MUST HAVE THIS LINE INSIDE THE HTML TO PROPERLY DISPLAY THE SCORE
+            //<div id="score">Score: 0</div>
+
+            //document.getElementById(" ").innerText = "Score: " + score.score;
+            //call the next question
+        }
+
+    }
 }
