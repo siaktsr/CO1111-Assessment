72a2086c0ef889729ac82d2709b0b450bdecb527
athanasios44 <aantoniou16@uclan.ac.uk>
2026-02-19 16:48:00 +0200

    Visible Leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,25 +1,40 @@
  import{
-        fetchLeaderboard,
-    }from"./api.js"
+         fetchLeaderboard,
+     }from"./api.js"
 
 
-async function initLeaderboard() {
+ async function initLeaderboard() {
 
-    const stored = localStorage.getItem("treasureHuntSession");
+     const stored = localStorage.getItem("treasureHuntSession");
 
-    if (!stored){ 
-        console.error("No session found!");
-        return;
-    }
+     if (!stored) {
+         console.error("No session found!");
+         return;
+     }
 
-    const parsedSession = JSON.parse(stored);
+     const parsedSession = JSON.parse(stored);
 
-    const leaderboardData = await fetchLeaderboard({
-        sessionId: parsedSession.sessionId,
-        sorted: true
-    });
+     const leaderboardData = await fetchLeaderboard({
+         sessionId: parsedSession.sessionId,
+         sorted : true
+     });
 
-    console.log(leaderboardData);
-}
+     console.log(leaderboardData);
 
-initLeaderboard();
+     let display = document.getElementById("leaderboard");
+
+     for (let i = 0; i < leaderboardData.leaderboard.length; i++) {
+         let division = document.createElement("div");
+         division.id = "entry";
+         let numberDisplayed = i + 1;
+         let show = numberDisplayed + " . " + leaderboardData.leaderboard[i].player + " - " + leaderboardData.leaderboard[i].score;
+         division.textContent = show;
+         display.appendChild(division);
+     }
+    // if(leaderboardData.leaderboard.  ){}
+ }
+//
+await initLeaderboard();
+
+
+
