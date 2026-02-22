a9841466d973c3a22c0dacee2527dff07f65a6c6
athanasios44 <aantoniou16@uclan.ac.uk>
2026-02-22 11:34:32 +0200

    Refactor leaderboard rendering with createCard function
    Refactor leaderboard display to use createCard function for better structure and readability.

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -3,6 +3,31 @@
      }from"./api.js"
 
 
+
+
+ function createCard(index , player , score ){
+    const card = document.createElement("div");
+    card.classList.add("entry");
+
+    const position = document.createElement("span");
+    position.textContent = index + 1 + ".  ";
+
+    const playerName = document.createElement("span");
+    playerName.textContent = player ;
+
+    const playerScore = document.createElement("span");
+    playerScore.textContent = score;
+
+
+    card.appendChild(position);
+    card.appendChild(playerName);
+    card.appendChild(playerScore);
+
+    return card;
+ }
+
+
+
  async function initLeaderboard() {
 
      const stored = localStorage.getItem("treasureHuntSession");
@@ -19,19 +44,14 @@
          sorted : true
      });
 
-     console.log(leaderboardData);
-
      let display = document.getElementById("leaderboard");
 
      for (let i = 0; i < leaderboardData.leaderboard.length; i++) {
-         let division = document.createElement("div");
-         division.id = "entry";
-         let numberDisplayed = i + 1;
-         let show = numberDisplayed + " . " + leaderboardData.leaderboard[i].player + " - " + leaderboardData.leaderboard[i].score;
-         division.textContent = show;
-         display.appendChild(division);
+
+        const card = createCard(i , leaderboardData.leaderboard[i].player , leaderboardData.leaderboard[i].score);
+        display.appendChild(card);
      }
-    // if(leaderboardData.leaderboard.  ){}
+
  }
 //
 await initLeaderboard();
