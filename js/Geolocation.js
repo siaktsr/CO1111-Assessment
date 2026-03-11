a2a32ed7d0ceb9a1fac86c515664af31ea1b65d3
athanasios44 <aantoniou16@uclan.ac.uk>
2026-03-11 11:30:43 +0200

    Testing Code

--- js/Geolocation.js
+++ js/Geolocation.js
@@ -7,20 +7,32 @@ import {
 import {
     CheckAndDisplay
 }from "./sessionEnd.js";
+import {updateScore} from "./score";
 
-//Import from resumeSession
-import{
-    findAndSaveSessionId,
-}from "./resumeSession.js";
+let sessionId;
+// Initialization
+init();
+
+function init() {
+    const StoredData = localStorage.getItem("treasureHuntSession");
+
+    if (!StoredData) {
+        alert("Session ID was not found. Please try again.");
+        window.location.href = "../test/test.html";
+    }
+    const sessionData = JSON.parse(StoredData);
+    sessionId = sessionData.sessionId;
+}
+
+document.addEventListener("answer-submitted", () => {
+    getLocation(sessionId);
+})
 
 
 
-await locationUpdates();
 
 
-
-
-export function showPosition(position){
+function showPosition(position){
     //Get the latitude
     let lat = position.coords.latitude;
     //Get the longitude
@@ -28,43 +40,55 @@ export function showPosition(position){
     return {lat,long};
 }
 
-//alert if browser does not allow location track
-export function CheckLocation(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else{
-        alert("Geolocation is not supported by this browser.");
-    }
-}
 
-export async function getLocation(){
-    //Get the position, longitude and latitude
-    let position = position;
-    let pos = showPosition(position);
 
-    //Get the Session ID
-    let sessionID = await findAndSaveSessionId();
-
-try{
-    let data = await updateLocation(sessionID, pos[0], pos[1]);
-    console.log(data);
-}
-catch(err){
-    alert("Unknown error: " + err);
-}
-
-}
-
-export async function locationUpdates(){
-
-    let sessionId = await findAndSaveSessionId();
-    if(sessionId === null){
-        alert("No session found!");
+async function getLocation(sessionId){
+    if(!navigator.geolocation){
+        alert("Geolocation is not supported.Please switch to different browser.");
         return;
     }
-    while (!await CheckAndDisplay(sessionId)) {
-        setInterval(getLocation,120000);
-        console.log("Hunt still Active...");
-    }
+    navigator.geolocation.getCurrentPosition(async (position) => {
+        //Get the position, longitude and latitude
+        let pos = showPosition(position);
+
+        try{
+            let data = await updateLocation(sessionId, pos.lat, pos.long);
+            console.log(data.message);
+            document.getElementById("currentLocation").innerHTML = "Location : " + data.message;
+        }
+        catch(err){
+            alert("Unknown error: " + err);
+        }
+    });
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export async function locationUpdates(sessionId){
+//
+//     if(sessionId === null){
+//         alert("No session found!");
+//         return;
+//     }
+//     while (!await CheckAndDisplay(sessionId)) {
+//         document.getElementById("currentLocation").innerText = "Location:" + setInterval(getLocation,120000);
+//         console.log("Hunt still Active...");
+//     }
+// }
