57231603791ef84fda70d9abd09f9fcbf7133c32
athanasios44 <aantoniou16@uclan.ac.uk>
2026-03-22 18:25:56 +0200

    Update resumeSession.js
    Added function that asks the user if they want to continue their session if it exists. If not displays an error

--- js/resumeSession.js
+++ js/resumeSession.js
@@ -1,65 +1,31 @@
-//Import from API
+//import from modals.js
 import {
-    startSession,
-    fetchQuestion
-} from "./api.js";
+    displayError
+} from "../js/modals.js";
 
-//start new session from API and save it in local storage
-export async function startNewSession(player,app,treasureHuntId) {
-    const start = await startSession(player,app,treasureHuntId);
-    const id = start.sessionId;
-    let saveSession = localStorage.setItem("SessionId",JSON.stringify(id));
-    await loadNextQuestion(id);
-}
 
-// Initialization
-init();
-let sessionId;
-function init() {
-    const StoredData = localStorage.getItem("treasureHuntSession");
+function checkSession() {
+    //get the session id
+    const sessionID = localStorage.getItem("treasureHuntSession");
 
-    if (!StoredData) {
-        alert("Session data not found.");
-        window.location.href = "../test/test.html";
-    }
-    const sessionData = JSON.parse(StoredData);
-    sessionId = sessionData.sessionId;
-}
+    //check if it doesnt exist display error
+    if (!sessionID) {
+       displayError("No existing session ID");
+       //if it exists and user presses YES
+    } else {
+        const userAgreement = confirm("There is an existing treasure Hunt Do you want to resume?");
 
-//On reload check
-window.onload =async function reloadPage(sessionId) {
-
-        //No session found throw error
-        if (!sessionId) {
-            console.log("Error no Session Id Found");
-        }
-        //Load the question
-        else{
-            await loadNextQuestion(sessionId);
-        }
-}
-
-//Function that loads the next question from API
-export async function loadNextQuestion(sessionId) {
-    try{
-        //Fetch Question from API
-        let question = await fetchQuestion(sessionId);
-        //If no question found
-        if (!question){
-            console.error("Error fetching Question");
-        }
-        //If all questions are completed display message
-        else if(question.completed){
-            console.log("Congratulations Hunter! You completed the Treasure Hunt!");
-        }
-        //Show the next question
-        else{
-            console.log(question);
-            console.log(question.questionText);
+        //redirect to questions for user to continue playing
+        if (userAgreement) {
+            window.location.href="../html/questions.html";
+            //else do nothing
+        } else {
+            console.log("User Decided to not resume their previous session.");
         }
     }
-    //Display Error
-    catch (error) {
-        console.error("Unknown Error");
-    }
 }
+
+//get the button id
+let resumeButton = document.getElementById("resumeSession");
+//when pressed call checkSession Function
+resumeButton.addEventListener("click", checkSession);
